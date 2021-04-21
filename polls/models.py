11b4157d9dba2c2aa973f2d2ca96b78019738178
Andy Xia <118010339@link.cuhk.edu.cn>
2021-04-21 22:29:02 +0800

    feat: change the user identify mode(use user info in the db instead of the stable data)
    Signed-off-by: Andy Xia <118010339@link.cuhk.edu.cn>

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class StudentInfo(models.Model):
+    stu_id = models.CharField(primary_key=True, max_length=20)
+    stu_name = models.CharField(max_length=20)
+    stu_pwd = models.CharField(max_length=20)
+    stu_email = models.CharField(max_length=50)
+    stu_course = models.CharField(max_length=100)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def toLogin_view(request):
@@ -8,8 +9,12 @@ def toLogin_view(request):
 def Login_view(request):
     user = request.POST.get("user", '')
     pwd = request.POST.get("pwd", '')
-    if user=="Andy Xia" and pwd=="andy123":
-        return HttpResponse("登陆成功")
+    if user and pwd:
+        c = StudentInfo.objects.filter(stu_name=user,stu_pwd=pwd).count()#获取models里studentinfo类对象的值
+        if c >= 1:
+            return HttpResponse("登陆成功")
+        else:
+            return HttpResponse("用户名或密码错误")
     else:
-        return HttpResponse("用户名或密码错误")
+        return HttpResponse("用户名或密码为空")
     # return render(request, 'index.html')
