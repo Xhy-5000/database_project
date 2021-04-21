330203dc1fc0ae4081316e768a9792cf4d19e2e7
Andy Xia <118010339@link.cuhk.edu.cn>
2021-04-22 03:30:54 +0800

    implement the register function
    Signed-off-by: Andy Xia <118010339@link.cuhk.edu.cn>

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.toLogin_view),
     path('index/', views.Login_view),
+    path('toregister/',views.toregister_view),
+    path('register/',views.register_view),
 ]
--- polls/views.py
+++ polls/views.py
@@ -18,3 +18,25 @@ def Login_view(request):
     else:
         return HttpResponse("用户名或密码为空")
     # return render(request, 'index.html')
+
+def toregister_view(request):
+    return render(request, 'register.html')
+
+def register_view(request):
+    user = request.POST.get("user", '')
+    pwd = request.POST.get("pwd", '')
+    confirm_pwd = request.POST.get("confirm_pwd", '')
+    email = request.POST.get("email", '')
+    course = request.POST.get("course", '')
+    if user and pwd and confirm_pwd and email and course:
+        c = StudentInfo.objects.filter(stu_name=user).count()
+        if c:
+            return HttpResponse("用户名已存在")
+        elif pwd != confirm_pwd:
+            return HttpResponse("密码不一致")
+        else:
+            stu = StudentInfo(stu_id=id,stu_name=user,stu_pwd=pwd,stu_email=email,stu_course=course)
+            stu.save()
+            return HttpResponse("注册成功")
+    else:
+        return HttpResponse("请补全信息")
