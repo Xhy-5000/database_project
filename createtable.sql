c9de172f20338fc8f6f68c2706ad427f76a34f4b
Andy Xia <118010339@link.cuhk.edu.cn>
2021-04-22 15:40:35 +0800

    Add sql files for initialize the database
    Signed-off-by: Andy Xia <118010339@link.cuhk.edu.cn>

---
+++ createtable.sql
@@ -0,0 +1,8 @@
+use stu_info;
+create table polls_studentinfo(
+	stu_id varchar(20),
+    stu_name varchar(20),
+    stu_pwd varchar(20),
+    stu_email varchar(50),
+    stu_course varchar(100)
+);
