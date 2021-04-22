9f281df3fb28b056c0f9a7b567c200d95deb394b
Andy Xia <118010339@link.cuhk.edu.cn>
2021-04-22 21:10:36 +0800

    Implement the initial insert with user role
    Signed-off-by: Andy Xia <118010339@link.cuhk.edu.cn>

--- polls/models.py
+++ polls/models.py
@@ -7,3 +7,4 @@ class StudentInfo(models.Model):
     stu_pwd = models.CharField(max_length=20)
     stu_email = models.CharField(max_length=50)
     stu_course = models.CharField(max_length=100)
+    stu_role = models.CharField(max_length=1)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -15,32 +16,43 @@ def Login_view(request):
     if user and pwd:
         c = StudentInfo.objects.filter(stu_name=user,stu_pwd=pwd).count()#获取models里studentinfo类对象的值
         if c >= 1:
-            return HttpResponse("登陆成功")
+            return render(request, 'index.html')
         else:
-            return HttpResponse("用户名或密码错误")
+            messages.success(request, "用户名或密码错误")
+            return render(request, 'login.html')
     else:
-        return HttpResponse("用户名或密码为空")
-    # return render(request, 'index.html')
+        messages.success(request, "用户名或密码为空")
+        return render(request, 'login.html')
+
 
 def toregister_view(request):
     return render(request, 'register.html')
 
 def register_view(request):
+    role = request.POST.get("role", '')
     user = request.POST.get("user", '')
     pwd = request.POST.get("pwd", '')
     confirm_pwd = request.POST.get("confirm_pwd", '')
     email = request.POST.get("email", '')
     stu_id = request.POST.get("id", '')
     course = request.POST.get("course", '')
-    if user and pwd and confirm_pwd and email and id and course:
+    if role and user and pwd and confirm_pwd and email and id and course:
         c = StudentInfo.objects.filter(stu_name=user).count()
         if c:
-            return HttpResponse("用户名已存在")
+            messages.success(request, "用户名已存在")
+            return render(request, 'register.html')
         elif pwd != confirm_pwd:
-            return HttpResponse("密码不一致")
+            messages.success(request, "密码不一致")
+            return render(request, 'register.html')
         else:
-            stu = StudentInfo(stu_id=stu_id,stu_name=user,stu_pwd=pwd,stu_email=email,stu_course=course)
+            if role == "teacher":
+                stu_role = '1'
+            else:
+                stu_role = '0'
+            stu = StudentInfo(stu_id=stu_id,stu_name=user,stu_pwd=pwd,stu_email=email,stu_course=course,stu_role=stu_role)
             stu.save()
-            return HttpResponse("注册成功")
+            messages.success(request, '注册成功')
+            return render(request, 'login.html')
     else:
-        return HttpResponse("请补全信息")
+        messages.success(request, "请补全信息")
+        return render(request, 'register.html')
